103265f9e4676cd07415027c50a2fc5db6c1589a
Alexandre Carvalho <alexandrecarvalhoramos@gmail.com>
2019-12-03 13:12:34 -0400

    primeiro commit deposito de bebidas, estruturação do projeto

---
+++ src/main/java/com/depositobebidas/depositobebidas/DepositobebidasApplication.java
@@ -0,0 +1,13 @@
+package com.depositobebidas.depositobebidas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepositobebidasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepositobebidasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/depositobebidas/depositobebidas/DepositobebidasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depositobebidas.depositobebidas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepositobebidasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
